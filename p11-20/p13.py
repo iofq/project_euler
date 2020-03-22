35fe1215c7f55eb8a0b9c90d28075e5402d80ebd
iofq <cjriddz@protonmail.com>
2020-03-22 18:30:50 -0500

    p13

--- p11-20/p13.py
+++ p11-20/p13.py
@@ -4,5 +4,16 @@ Work out the first ten digits of the sum of the following one-hundred 50-digit n
 (see p13.txt)
 '''
 
+#read file to array, slice only first 11 digits
+def read_file(f="p13.txt"):
+    with open(f) as file:
+        lines = [int(line.rstrip()[0:11]) for line in file]
+    return lines
 
+def main():
+    n = read_file()
+    sum = 0
+    for i in n:
+        sum += i
+    return(str(sum)[0:10])
 
